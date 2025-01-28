467c78a920100726d1c56d89bf3b259a263f60cb
Bradford Gill <bradfordgill@Bradfords-MacBook-Pro.local>
2025-01-28 15:46:22 -0700

    Add Model

--- src/model.py
+++ src/model.py
@@ -2,8 +2,8 @@ from layers import (AddNorm, FeedForward, MultiHeadAttention, InputEncoding, Out
 import torch.nn as nn
 
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim, embed_dim, n_heads, num_encoder_layers, num_decoder_layers, ff_dim, max_seq_length, num_classes):
         pass
     
-    def forward(self):
+    def forward(self, source, targer):
         pass
