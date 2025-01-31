40878ecc042c673353f7256b7806ac8276364c0f
Jack Freeman <110849539+jackfreem@users.noreply.github.com>
2025-01-31 14:17:02 -0500

    Implement Encoder and EncoderBlock (#10)

--- src/model.py
+++
@@ -1,9 +0,0 @@
-from layers import (AddNorm, FeedForward, MultiHeadAttention, InputEncoding, OutputEmbedding)
-import torch.nn as nn
-
-class Model(nn.Module):
-    def __init__(self):
-        pass
-    
-    def forward(self):
-        pass
